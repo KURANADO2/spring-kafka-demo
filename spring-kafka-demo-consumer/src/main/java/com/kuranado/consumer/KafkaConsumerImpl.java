ee98e4b30852d289ed29b0e1228d2f9908497988
kuranado <jingxinling1024@163.com>
2019-09-17 14:40:27 +0800

    feat: 验证 spring-kafka 消费出现异常情况下的消费位移提交

--- spring-kafka-demo-consumer/src/main/java/com/kuranado/consumer/KafkaConsumerImpl.java
+++ spring-kafka-demo-consumer/src/main/java/com/kuranado/consumer/KafkaConsumerImpl.java
@@ -8,6 +8,8 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
+
 /**
  * @Author: Xinling Jing
  * @Date: 2019-09-16 15:41
@@ -16,14 +18,24 @@ import org.springframework.stereotype.Component;
 @Component
 public class KafkaConsumerImpl {
 
+    private static Random random;
+
+    static {
+        random = new Random();
+    }
+
     @KafkaListener(topics = {"#{'${kafka.testTopic}'}"})
     public void listenTestTopic(ConsumerRecord<String, String> consumerRecord) {
         // 处理消息
         try {
-            log.info("开始处理消息...key:[{}],value:[{}],offset:[{}],timestamp:[{}],topic:[{}],partition:[{}],", consumerRecord.key(),
+            log.info("开始处理消息...key:[{}],value:[{}],offset:[{}],timestamp:[{}],topic:[{}],partition:[{}],",
+                consumerRecord.key(),
                 consumerRecord.value(), consumerRecord.offset(), consumerRecord.timestamp(), consumerRecord.topic(),
                 consumerRecord.partition());
             Thread.sleep(2000);
+            if (random.nextBoolean()) {
+                int a = 1 / 0;
+            }
             log.info("处理消息完成.key:[{}]", consumerRecord.key());
         } catch (InterruptedException e) {
             log.error("线程休眠出现异常", e);
@@ -32,5 +44,9 @@ public class KafkaConsumerImpl {
         } catch (Exception e) {
             log.error("消息处理出现未知异常", e);
         }
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
     }
 }
